e1cf77abb36656e851726d1345c13c14a1d084ed
Alexey Serafimov <alexey.serafimov.consultant@nielsen.com>
2018-09-11 14:15:56 +0300

    unit test demo

--- src/app/tasks.service.spec.ts
+++ src/app/tasks.service.spec.ts
@@ -12,4 +12,45 @@ describe('TasksService', () => {
   it('should be created', inject([TasksService], (service: TasksService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('this.addTask("New task name") should add to task with name "New task name" to this.tasks',
+    inject([TasksService],
+      (service: TasksService) => {
+        service.tasks = []
+        let taskName: string = "New task name";
+        service.addTask(taskName);
+        expect(service.tasks.find(task => task.text === taskName)).toBeTruthy();
+      }
+    )
+  );
+
+  // it('this.addTask("Add task that is name already exists and NOT finished") should NOT add task to this.tasks',
+  //   inject([TasksService],
+  //     (service: TasksService) => {
+  //       service.tasks = []
+  //       let taskName: string = "Add task that is name already exists and NOT finished";
+  //       service.addTask(taskName);
+  //       service.addTask(taskName);
+  //       expect(service.tasks.filter(task => task.text === taskName).length).toEqual(1);
+  //     }
+  //   )
+  // );
+
+  it('this.addTask("Add task that is name already exists and finished") should add task to this.tasks',
+    inject([TasksService],
+      (service: TasksService) => {
+        service.tasks = []
+        let taskName: string = "Add task that is name already exists";
+        let taskId = service.tasks.length;
+        service.addTask(taskName);
+        service.completeTask(taskId);
+        service.addTask(taskName);
+        expect(service.tasks.filter(task => task.text === taskName).length).toEqual(2);
+        expect(service.tasks.filter(task => task.text === taskName && task.complete === true).length).toEqual(1);
+        expect(service.tasks.filter(task => task.text === taskName && task.complete === false).length).toEqual(1);
+
+      }
+    )
+  );
+
 });
